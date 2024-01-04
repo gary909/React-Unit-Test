43bf2bb296b3072ef58074955a5577e4b2c36e7b
whg <whg@makrosolutions.com>
2024-01-04 12:49:05 +0100

    added describe

--- HomePageTest.test.jsx
+++ HomePageTest.test.jsx
@@ -5,19 +5,20 @@ import HomePage from './HomePage'; // The page we'll be testing
 // My first test!
 // This simple test searches to see if the text 'dashboard' is present on the page
 
-test('searches if dashboard text is present on page', () => {
-    // Arrange
-    render( // you need to wrap components that use 'use-navigate' in a router context during the testing
-        <BrowserRouter> 
-            <HomePage />
-        </BrowserRouter>
-    );
+describe('Async component', () => {
+    test('searches if dashboard text is present on page', () => {
+        // Arrange
+        render( // you need to wrap components that use 'use-navigate' in a router context during the testing
+            <BrowserRouter> 
+                <HomePage />
+            </BrowserRouter>
+        );
 
-    // Act 
-    // ...Nothing
+        // Act 
+        // ...Nothing
 
-    // Assert
-    const dashBoardTextElement = screen.getByText('dashboard', {exact: false}); // search for 'dashboard text'
-    expect(dashBoardTextElement).toBeInTheDocument(); // Is text present?
+        // Assert
+        const dashBoardTextElement = screen.getByText('dashboard', {exact: false}); // search for 'dashboard text'
+        expect(dashBoardTextElement).toBeInTheDocument(); // Is text present?
+    });
 });
-
