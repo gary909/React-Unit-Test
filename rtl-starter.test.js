7f34b1b832f5a24b9261819802507031d82740b8
whg <whg@makrosolutions.com>
2024-01-08 10:30:56 +0100

    first test from course

---
+++ rtl-starter.test.js
@@ -0,0 +1,26 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
+
+test("shows 6 products by default", async () => {
+  render(<App />);
+
+  const headings = await screen.findAllByRole("heading");
+  expect(headings).toHaveLength(6);
+});
+
+test("clicking on the button loads 6 more products", async () => {
+  render(<App />);
+
+  const button = await screen.findByRole("button", {
+    name: /load more/i
+  });
+  // wait for user to click button
+  await user.click(button);
+
+  // wait for page to load
+  await waitFor(async () => {
+    const headings = await screen.findAllByRole("heading");
+    expect(headings).toHaveLength(12); // 6 more headings should load, giving us 12
+  });
+});
